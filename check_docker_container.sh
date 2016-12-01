4b1b6a2825a9d3a800a80005349599127ca76953
MikkoE <mikko.eberhardt@haw-hamburg.de>
2016-12-01 13:43:48 +0100

    Insert pipe to slacktee in check_docker_container

--- check_docker_container.sh
+++ check_docker_container.sh
@@ -17,23 +17,25 @@ CONTAINER=$1
 RUNNING=$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
 
 if [ $? -eq 1 ]; then
-  echo "UNKNOWN - $CONTAINER does not exist."
+  echo "UNKNOWN - $CONTAINER does not exist." | ./slacktee.sh
   exit 3
 fi
 
 if [ "$RUNNING" == "false" ]; then
-  echo "CRITICAL - $CONTAINER is not running."
+  echo "CRITICAL - $CONTAINER is not running." | ./slacktee.sh
   exit 2
 fi
 
 GHOST=$(docker inspect --format="{{ .State.Ghost }}" $CONTAINER)
 
 if [ "$GHOST" == "true" ]; then
-  echo "WARNING - $CONTAINER has been ghosted."
+  echo "WARNING - $CONTAINER has been ghosted." | ./slacktee.sh
   exit 1
 fi
 
 STARTED=$(docker inspect --format="{{ .State.StartedAt }}" $CONTAINER)
 NETWORK=$(docker inspect --format="{{ .NetworkSettings.IPAddress }}" $CONTAINER)
 
-echo "OK - $CONTAINER is running. IP: $NETWORK, StartedAt: $STARTED"
+echo "OK - $CONTAINER is running. IP: $NETWORK, StartedAt: $STARTED" 
+
+exit 0
